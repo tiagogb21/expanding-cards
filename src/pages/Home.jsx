a20b564653eb6dc75a0c87a13f2ae10c1d3db277
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-24 11:39:29 -0300

    adicionando estilo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import getInfoApi from '../services/fetch/api';
+
+import './Home.css';
 
 function Home() {
   const [data, setData] = useState([]);
   const [inputSearch, setInputSearch] = useState('');
+  const [targetId, setTargetId] = useState(0);
 
   const handleClick = async () => {
     const getData = await getInfoApi(inputSearch || 'cat');
     return setData(getData.response.results);
   };
 
-  console.log(data);
+  useEffect(() => {
+    async function getInfo() {
+      const getData = await getInfoApi('cat');
+      return setData(getData.response.results);
+    }
+    getInfo();
+  }, []);
+
+  const teste = (id) => (id === targetId ? 'active' : '');
 
   return (
-    <>
-      <label htmlFor="input-search">
-        <input
-          type="text"
-          id="input-search"
-          value={inputSearch}
-          onChange={({ target }) => setInputSearch(target.value)}
-        />
-      </label>
-      <button type="button" onClick={() => handleClick()}>
-        Search
-      </button>
-      {data !== undefined &&
-        data.length > 0 &&
-        data.map((item) => (
-          <img src={item.urls.full} alt={item.urls.alt_description} />
-        ))}
-    </>
+    <section className="container">
+      <section className="search__container">
+        <label htmlFor="input-search">
+          <input
+            type="text"
+            id="input-search"
+            value={inputSearch}
+            onChange={({ target }) => setInputSearch(target.value)}
+          />
+        </label>
+        <button type="button" onClick={() => handleClick()}>
+          Search
+        </button>
+      </section>
+      <section className="images__container">
+        {data !== undefined &&
+          data.length > 0 &&
+          data.map((item, id) => (
+            <div key={item.id} className="panel">
+              <img
+                className={`panel ${teste(id)}`}
+                src={item.urls.full}
+                alt={item.alt_description}
+              />
+              <h3>{item.alt_description}</h3>
+            </div>
+          ))}
+      </section>
+    </section>
   );
 }
 
